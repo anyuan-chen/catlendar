4497e5729326fc949675337a9ef693b494bb8a45
Andrew Chen <andrew.chen.anyuan@gmail.com>
2021-09-08 10:36:24 -0400

    the best i can do for now :(

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,15 @@ import AngleArrow from "../components/AngleArrow";
 import CatHands from "../components/CatHands";
 import Navbar from "../components/Navbar";
 import PolarPaw from "../components/PolarPaw";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <Navbar></Navbar>
-      <div className="grid grid-cols-12 grid-rows-6 flex-grow">
+      <div className="grid grid-cols-12 grid-rows-6">
         {/* pink circle */}
-        <div className="bg-pinkLight mt-10 ml-16 row-start-1 row-span-4 col-start-1 col-span-4 z-10 rounded-full flex justify-between flex-col">
+        <div className="bg-pinkLight mt-10 ml-16 row-start-1 row-span-3 col-start-1 col-span-4 z-10 rounded-full flex justify-between flex-col">
           <div className="flex flex-col items-center">
             <h2 className="pt-16 font-primary text-xl">Intrdoucing your new</h2>
             <h2 className="pt-2 font-primary text-2xl font-extrabold">
@@ -44,23 +45,8 @@ export default function Home() {
           className="bg-gray-50 col-start-2 row-start-2 rounded-3xl row-span-4 col-span-10 "
           id="greyBox"
         ></div>
-        <div className="row-end-7 col-start-6 row-span-4 z-10">
-          <svg
-            width="783"
-            height="524"
-            viewBox="0 0 783 524"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <ellipse
-              cx="391.5"
-              cy="390.038"
-              rx="391.5"
-              ry="389.038"
-              transform="rotate(7.96623 391.5 390.038)"
-              fill="#FFCACA"
-            />
-          </svg>
+        <div className="z-10 row-start-3 row-span-4 col-start-5 col-span-6">
+          <img src={"./img/PusheenCircles.svg"} alt="kekw"></img>
         </div>
       </div>
     </div>
